0d92c5bb07867a6503f7a880cd58fb360ace5875
bazu15 <bazu15@users.noreply.github.com>
2019-04-01 20:21:20 -0400

    assgn

--- StackL.cpp
+++ StackL.cpp
@@ -1,1 +1,20 @@
 #include "StackL.h"
+
+void Stack::pop() {
+	data.remove(data.size());
+}
+
+int Stack::top() {
+	return data.get(data.size());
+}
+
+void Stack::clear() {
+	data.clear();
+}
+int Stack::size() {
+	return data.size();
+}
+
+void Stack::push(int k) {
+	data.insert(k, data.size()+1);
+}
