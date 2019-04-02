1ba7bf1536be6e6df8e783b2eb273357994f59ee
bazu15 <bazu15@users.noreply.github.com>
2019-04-01 20:19:13 -0400

    assgn

--- ListClient.cpp
+++ ListClient.cpp
@@ -10,5 +10,12 @@ int main()
 
  //Do some stuff with L1, L2, ...
  // ...
+for(int k = 0; k < 10; k++)
+	 	L1.insert(k, k+1);
+	 L1.display();
+	 cout<<L1.get(6)<<endl;
+	 L1.clear();
+	 L1.insert(3, 1);
+	 L1.display();
 
 }
